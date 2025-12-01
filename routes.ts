b77ca541c6af34b512cd3281844b9c19d7545d23
AX2 <186868343+ax2-dev@users.noreply.github.com>
2025-12-01 14:04:11 -0500

    Reimplemented the routes. Converted to Typescript.

--- routes.ts
+++ routes.ts
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
+import { responseItemType, weatherDetailType, weatherQueryInterface } from "./custom";
 const routeHello = () => "Hello World!";
-const routeAPINames = async () => {
+const routeAPINames = async (): Promise<string> => {
     const url = "https://www.usemodernfullstack.dev/api/v1/users";
-    let data;
+    let data: responseItemType[];
     try {
         const response = await fetch(url);
-        data = await response.json();
+        data = (await response.json()) as responseItemType[];
     } catch (err) {
         return err;
     }
@@ -14,4 +15,14 @@ const routeAPINames = async () => {
         .join("<br>");
     return names;
 };
+
+const routeWeather = (query: weatherQueryInterface): weatherDetailType=> queryWeatherData(query);
+const queryWeatherData = (query: weatherQueryInterface): weatherDetailType=>{
+    return {
+        zipcode:query.zipcode,
+        weather:"sunny",
+        temp:35,
+    };
+};
+
 export { routeHello, routeAPINames };
